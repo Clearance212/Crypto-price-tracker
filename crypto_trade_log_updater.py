42ae67e5d72ba56ed593db2d6da982aaf32cf5ec
Clarence <99848585+Clearance212@users.noreply.github.com>
2023-11-26 15:38:59 -0800

    Add files via upload

---
+++ crypto_trade_log_updater.py
@@ -0,0 +1,14 @@
+
+coins = ["Sol", "Tron", "Lite", "Algo", "Rvn", "Zcash", "Eth"]
+
+with open("cryptoorg.txt", "a") as file:
+    d = input("Date today: \n")
+    file.write(d)
+    file.write('\n')
+    file.write("------------------\n")
+    for i in coins:
+        a = input(i + ": ")
+        file.write(a)
+        file.write('\n')
+
+    file.write('\n')
